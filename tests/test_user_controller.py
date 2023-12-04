9eb200535a9a408ea81ec3cff89f6d89f7b8b8b0
Artem Chemodanov <artem_chemodanov2000@mail.ru>
2023-12-04 14:00:00 +0300

    add status_codes

--- tests/test_user_controller.py
+++ tests/test_user_controller.py
@@ -18,22 +18,22 @@ def test_register_user_negative(user_controller, login, password):
 
 
 def test_get_user_info(user_controller):
-    user_controller.post_new_user(user_controller.login, user_controller.password)
+    user_controller.post_new_user(user_controller.login, user_controller.password, status_code=201)
     user_controller.auth()
-    user_controller.get_user_info()
+    user_controller.get_user_info(status_code=200)
 
 
 def test_put_new_user_password(user_controller):
-    user_controller.post_new_user(user_controller.login, user_controller.password)
+    user_controller.post_new_user(user_controller.login, user_controller.password, status_code=201)
     user_controller.auth()
-    user_controller.put_user_password("newpassword")
+    user_controller.put_user_password("newpassword", status_code=200)
 
 
 def test_delete_user(user_controller):
     if user_controller.login in user_controller.get_users_info().info:
         if user_controller.can_auth():
             user_controller.auth()
-            user_controller.delete_user()
+            user_controller.delete_user(status_code=200)
         else:
             print("---User can't auth---")
     else:
@@ -41,4 +41,4 @@ def test_delete_user(user_controller):
 
 
 def test_get_all_users_logins(user_controller):
-    user_controller.get_users_info()
+    user_controller.get_users_info(status_code=200)
