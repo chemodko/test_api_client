13adc9090d25a1083f0cb97918a4c236eb9c6554
Artem Chemodanov <artem_chemodanov2000@mail.ru>
2023-12-06 19:00:09 +0300

    update authentication tests

--- src/controllers/jwt_authentication.py
+++ src/controllers/jwt_authentication.py
@@ -1,18 +1,13 @@
-from typing import Union
-
 from src.http_api_client import HttpApiClient
-from src.models.jwt_authentication import AuthenticationResponse, UnauthorizedError
+from src.models.jwt_authentication import AuthenticationResponse
 
 
 class JwtAuthenticationApiClient(HttpApiClient):
-    def post_create_auth_token(self, login: str = None, password: str = None, status_code: int = 200) -> Union[AuthenticationResponse, UnauthorizedError]:
+    def post_create_auth_token(self, login: str = None, password: str = None, status_code: int = 200, model=AuthenticationResponse):
         log_in_data = {
             "username": login,
             "password": password
         }
         token_resp = self.post(f"{self.base_url}/login", json=log_in_data, status_code=status_code)
-        if status_code == 200:
-            return AuthenticationResponse(**token_resp)
-        elif status_code == 401:
-            return UnauthorizedError(**token_resp)
+        return model(**token_resp)
 
--- tests/tests_jwt_authentication/test_jwt_authentication.py
+++ tests/tests_jwt_authentication/test_jwt_authentication.py
@@ -1,15 +1,16 @@
 from src.controllers.jwt_authentication import JwtAuthenticationApiClient
 from src.utils.randoms import random_string
+from src.models.jwt_authentication import UnauthorizedError
 
 
-def test_authentication_positive():
-    jwt_auth_controller = JwtAuthenticationApiClient()
-    jwt_auth_controller.post_create_auth_token(login="chemodko2", password="password", status_code=200)  # existing user
+class TestJwtAuthentication:
+    def test_authentication(self):
+        jwt_auth_controller = JwtAuthenticationApiClient()
+        resp = jwt_auth_controller.post_create_auth_token(login="chemodko2", password="password",
+                                                          status_code=200)  # existing user
+        assert len(resp.token) > 0
 
-
-def test_authentication_negative():
-    jwt_auth_controller = JwtAuthenticationApiClient()
-    jwt_auth_controller.post_create_auth_token(login=random_string(), password=random_string(), status_code=401)  # non-existing user
-
-
-
+    def test_authentication_unauthorized(self):
+        jwt_auth_controller = JwtAuthenticationApiClient()
+        jwt_auth_controller.post_create_auth_token(login=random_string(), password=random_string(),
+                                                   status_code=401, model=UnauthorizedError)  # non-existing user
